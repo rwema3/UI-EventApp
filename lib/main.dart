47bd209aa10bc8dbb807d63ce0fe8e00df071dec
rwema3 <rwemadominik3@gmail.com>
2022-02-03 10:04:43 +0200

    UI-

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:uvento/home.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
