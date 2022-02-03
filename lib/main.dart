1a5b51ca2b34a80f49470013e82675b0addb6061
rwema3 <rwemadominik3@gmail.com>
2022-02-03 10:08:02 +0200

    UI-
    UI-

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:uvento/home.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This 
+  // Thi
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
