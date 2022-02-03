92e5398202be5903aa733a5ebaf1249df00a9c83
rwema3 <rwemadominik3@gmail.com>
2022-02-03 10:12:01 +0200

    UI-
    UI-

--- lib/home.dart
+++ lib/home.dart
@@ -21,7 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   void initState() {
-    // TODO: implement initSta
+    // TODO: implement initStat
     super.initState();
     dates = getDates();
     eventsType = getEventTypes();
