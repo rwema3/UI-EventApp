6aa1616d156a3e10f5968c43da567e29c0fdb927
rwema3 <rwemadominik3@gmail.com>
2022-02-04 15:56:55 +0200

    EventApp

--- lib/home.dart
+++ lib/home.dart
@@ -21,7 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   void initState() {
-    // TODO: implemen
+    // TODO: impleme
     super.initState();
     dates = getDates();
     eventsType = getEventTypes();
